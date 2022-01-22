2d1d622d4a0c6987c657c4513caa2e925b3128a5
georgehuble <nevajno23@mail.ru>
2022-01-22 21:26:12 +0300

    fix

--- baskhomeBackend/cart/forms.py
+++ baskhomeBackend/cart/forms.py
@@ -5,8 +5,8 @@ class CartAddProductForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=999,
                                   label='',
                                   widget=forms.TextInput(attrs={
-                                  'type': 'text', 'value': '1',
-                                  'autocomplete': 'off'
+                                      'type': 'text', 'value': '1',
+                                      'autocomplete': 'off'
                                   }))
     update = forms.BooleanField(required=False,
                                 initial=False,
--- baskhomeBackend/shop/forms.py
+++ baskhomeBackend/shop/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Client, Product
+from .models import Client
 
 
 class ClientForm(forms.ModelForm):
